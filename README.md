485518e2f62150f0af4b6271e3f3e8be37bd75b2
rschouten97 <r.schouten@tools4ever.com>
2021-07-30 17:11:16 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# HelloID-Conn-SA-Target-Sync-AFAS-AD-Groups
-Scheduled task to sync AFAS data to AD groups
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+- [Table of Contents](#table-of-contents)
+- [Introduction](#introduction)
+- [Getting Started](#getting-started)
+- [Post-setup configuration](#post-setup-configuration)
+- [Getting help](#getting-help)
+- [HelloID Docs](#helloid-docs)
+
+
+## Introduction
+The interface to communicate with Profit is through a set of GetConnectors, which is component that allows the creation of custom views on the Profit data. GetConnectors are based on a pre-defined 'data collection', which is an existing view based on the data inside the Profit database. 
+
+For this connector we have created a default set, which can be imported directly into the AFAS Profit environment.
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+By using this connector you will have the ability to create AD groups based on data from the AFAS Profit HR system.
+
+Connecting to Profit is done using the app connector system. 
+Please see the following pages from the AFAS Knowledge Base for more information.
+
+[Create the APP connector](https://help.afas.nl/help/NL/SE/App_Apps_Custom_Add.htm)
+
+[Manage the APP connector](https://help.afas.nl/help/NL/SE/App_Apps_Custom_Maint.htm)
+
+[Manual add a token to the APP connector](https://help.afas.nl/help/NL/SE/App_Apps_Custom_Tokens_Manual.htm)
+
+
+The following GetConnectors are required by HelloID: 
+*	Tools4ever - HelloID - T4E_HelloID_OrganizationalUnits
+
+## Post-setup configuration
+The following items need to be configured according to your own environment
+ 1. Update the following [user defined variables](https://docs.helloid.com/hc/en-us/articles/360014169933-How-to-Create-and-Manage-User-Defined-Variables)
+<table>
+  <tr><td><strong>Variable name</strong></td><td><strong>Example value</strong></td><td><strong>Description</strong></td></tr>
+  <tr><td>AFASBaseUri</td><td>https://12345.rest.afas.online/ProfitRestServices</td><td>Base URI of the AFAS REST API endpoint for this environment</td></tr>
+<tr><td>AFASToken</td><td><token><version>1</version><data>D5R324DD5F4TRD945E530ED3CDD70D94BBDEC4C732B43F285ECB12345678</data></token></td><td>App token in XML format for this environment</td></tr>
+</table>
+
+## Getting help
+> _For more information on how to configure a HelloID PowerShell scheduled task, please refer to our [documentation](https://docs.helloid.com/hc/en-us/articles/115003253294-Create-Custom-Scheduled-Tasks) pages_
+
+> _If you need help, feel free to ask questions on our [forum](https://forum.helloid.com)
+
+## HelloID Docs
+The official HelloID documentation can be found at: https://docs.helloid.com/
---
+++ Sync AFAS Organizational Units to AD Groups.ps1
@@ -0,0 +1,117 @@
+#Region Script
+# AFAS Profit Parameters
+$baseUri = $AFASBaseUri
+$token = $AFASToken
+
+$path = "OU=OrganizationalUnits,OU=Security Groups,DC=enyoi,DC=local"
+$adGroupNamePrefix = "HelloID-Department-"
+$adGroupNameSuffix = ""
+$adGroupDescriptionPrefix = "Security Group for "
+$adGroupDescriptionSuffix = ""
+
+$debug = $true
+
+# Set TLS to accept TLS, TLS 1.1 and TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
+
+function Get-AFASConnectorData
+{
+    param(
+        [parameter(Mandatory=$true)]$Token,
+        [parameter(Mandatory=$true)]$BaseUri,
+        [parameter(Mandatory=$true)]$Connector,
+        [parameter(Mandatory=$true)][ref]$data
+    )
+
+    try {
+        $encodedToken = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($Token))
+        $authValue = "AfasToken $encodedToken"
+        $Headers = @{ Authorization = $authValue }
+
+        $take = 100
+        $skip = 0
+
+        $uri = $BaseUri + "/connectors/" + $Connector + "?skip=$skip&take=$take"
+        $counter = 0 
+        do {
+            if ($counter -gt 0) {
+                $skip += 100
+                $uri = $BaseUri + "/connectors/" + $Connector + "?skip=$skip&take=$take"
+            }    
+            $counter++
+            [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
+            $dataset = Invoke-RestMethod -Method GET -Uri $uri -ContentType "application/json" -Headers $Headers #-UseBasicParsing
+
+            foreach ($record in $dataset.rows) { $null = $data.Value.add($record) }
+
+        }until([string]::IsNullOrEmpty($dataset.rows))
+    } catch {
+        $data.Value = $null
+        throw $_
+    }
+}
+
+#region Supporting Functions
+function Get-ADSanitizeGroupName
+{
+    param(
+        [parameter(Mandatory = $true)][String]$Name
+    )
+    $newName = $name.trim();
+    $newName = $newName -replace ' - ','_'
+    $newName = $newName -replace '[`,~,!,#,$,%,^,&,*,(,),+,=,<,>,?,/,'',",;,:,\,|,},{,.]',''
+    $newName = $newName -replace '\[','';
+    $newName = $newName -replace ']','';
+    $newName = $newName -replace ' ','_';
+    $newName = $newName -replace '\.\.\.\.\.','.';
+    $newName = $newName -replace '\.\.\.\.','.';
+    $newName = $newName -replace '\.\.\.','.';
+    $newName = $newName -replace '\.\.','.';
+    return $newName;
+}
+#endregion Supporting Functions
+
+try{
+    Hid-Write-Status -Event "Information" -Message "Processing T4E_HelloID_OrganizationalUnits.."
+    $organizationalUnits = New-Object System.Collections.ArrayList
+    Get-AFASConnectorData -Token $token -BaseUri $baseUri -Connector "T4E_HelloID_OrganizationalUnits" ([ref]$organizationalUnits)
+
+    $departments = $organizationalUnits | Sort-Object ExternalId -Unique | Sort-Object ExternalId, DisplayName
+    foreach($department in $departments){
+        try{
+            # The names of security principal objects can contain all Unicode characters except the special LDAP characters defined in RFC 2253.
+            # This list of special characters includes: a leading space; a trailing space; and any of the following characters: # , + " \ < > ;
+            # A group account cannot consist solely of numbers, periods (.), or spaces. Any leading periods or spaces are cropped.
+            # https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2003/cc776019(v=ws.10)?redirectedfrom=MSDN
+            # https://www.ietf.org/rfc/rfc2253.txt
+            $ADGroupName = ("$adGroupNamePrefix$($department.ExternalId)$adGroupNameSuffix")
+            $ADGroupName = Get-ADSanitizeGroupName -Name $ADGroupName
+
+            $ADGroupParams = @{
+                Name                = $ADGroupName
+                SamAccountName      = $ADGroupName
+                GroupCategory       = "Security"
+                GroupScope          = "Universal"
+                DisplayName         = $ADGroupName
+                Path                = $path
+                Description         = "$adGroupDescriptionPrefix$($ADGroupName)$adGroupDescriptionSuffix"
+            }
+
+            $ADGroup = $null
+            $distinguishedName = "CN=$ADGroupName,$($ADGroupParams.Path)"
+            $ADGroup = Get-ADGroup -Filter { DistinguishedName -eq $distinguishedName }
+            if($ADGroup){
+                if($debug -eq $true){ Hid-Write-Status -Event "Warning" -Message "AD Group $($distinguishedName) already exists" }
+            }else{
+                $NewADGroup = New-ADGroup @ADGroupParams
+                if($debug -eq $true){ Hid-Write-Status -Event "Success" -Message "AD Group $($distinguishedName) created successfully" }
+            }
+        }catch{
+            Hid-Write-Status -Event "Error" -Message "$($ADGroupParams | Out-String)"
+            throw $_
+        }
+    }
+    Hid-Write-Summary -Event "Success" -Message "Successfully synchronized $($departments.Count) AFAS Organizational Units to AD Groups"
+}catch{
+    throw $_
+}
